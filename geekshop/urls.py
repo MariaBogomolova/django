e211089091d19e5ba090ca8e2077cbb2aa1db9c0
Maria <mariabo11197@gmail.com>
2024-05-01 19:42:20 +0500

    made media in urls and settings added products with admin panel

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from mainapp.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('products/', include('mainapp.urls', namespace='mainapp')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
